18c860f038a20bc4954012a48ce94b8815a7d4ca
Dimitar Gueorguiev <dimitar.gueorguiev@celect.com>
2021-06-21 07:33:34 -0400

    universal ref example update

--- universal_references/univer_ref01.cpp
+++ universal_references/univer_ref01.cpp
@@ -5,8 +5,51 @@
 #include <string>
 #include <vector>
 #include <map>
+#include <memory>
 
 
+struct SomeDataStructure {
+  std::string names[10];
+  float numbers[10];
+};
+
+class WidgetWithRValueRef {
+public:
+    WidgetWithRValueRef(WidgetWithRValueRef&& rhs)    // rhs is rvalue reference
+        : name(std::move(rhs.name)),
+          p(std::move(rhs.p))
+    {
+      // initialize something else
+    }
+    WidgetWithRValueRef(std::string&& newName, std::shared_ptr<SomeDataStructure>&& newData) :
+        name(std::move(newName)), p(std::move(newData)) 
+    {
+      // initialize something else
+    }
+private:
+    std::string name;
+    std::shared_ptr<SomeDataStructure> p;
+};
+
+
+class WidgetWithUniversalRef {
+public:
+    template<typename T, typename D>
+    WidgetWithUniversalRef(T&& newName, D&& newData) :
+        name(std::forward<T>(newName)), data(std::forward<T>(newData)) 
+    { 
+        // initialize something else
+    }
+    template<typename T>
+    void setName(T&& newName)  // newName is unversal reference 
+    { this->name = std::forward<T>(newName); }
+    template<typename D>
+    void setData(D&& newData)
+         { this->p = std::forward<D>(newData); } 
+       private:
+          std::string name;
+          std::shared_ptr<SomeDataStructure> p;
+};
 
 int main (int argc, const char* argv[]) {
 
@@ -48,26 +91,16 @@ int main (int argc, const char* argv[]) {
        // in a way that permits those functions to take advantage of the object's
        // rvalueness. The way to do that is to cast parameters bound to such 
        // objects to rvalues. For this purpose we use std::move.
+       SomeDataStructure* data = new SomeDataStructure();
 
-       struct SomeDataStructure {
-           std::string names[10];
-           float numbers[10];
-       };
 
-       class Widget {
-       public:
-           Widget(Widget&& rhs)    // rhs is rvalue reference
-            : name(std::move(rhs.name)),
-            p(std::move(rhs.p))
-           {
-             // initialize something else
-           }
-       private:
-           std::string name;
-           std::shared_ptr<SomeDataStructure> p;
-       };
 
-     }
+
+    }
+
+    {
+
+    }
 
     return 0;
 }
