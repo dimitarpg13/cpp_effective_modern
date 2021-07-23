addd472cfdbb7afc6d91ab1b9b63b3afd2e91989
Dimitar Gueorguiev <dimitar.gueorguiev@celect.com>
2021-07-23 05:18:32 -0400

    template type deduction example

--- deducing_types/template_type_deduction01.cpp
+++ deducing_types/template_type_deduction01.cpp
@@ -104,9 +104,86 @@
 //  That's doubly unusual. First, it is the only situation in template type deduction
 //  where T is deduced to be a reference. Second, although ParamType is declared
 //  using the syntax for an rvalue reference, its deduced type is an lvalue reference.
-//  * if expr is an rvalue the "normal" (Case 1) rules apply 
+//  * if expr is an rvalue the "normal" (Case 1) rules apply
+//
+//  For example:
+//    template<typename T>
+//    void f(T&& param);  // param is now a universal reference
+//
+//    int x = 27; 
+//    const int cx = x;
+//    const int& rx = x;
+//    f(x);  // x is lvalue so T is int&,
+//           // param's type is also int&
+//
+//    f(cx); // cx is lvalue, so T is const int&
+//           // param's type is also const int&
+//
+//    f(rx); // rx is lvalue, so T is const int&,
+//           // param's type is also const int&
+//
+//    f(27); // 27 is rvalue, so T is int,
+//           // param's type is therefore int&&
+//
+//
+//  Case 3: ParamType Is Neither A Pointer Nor A Reference
+//
+//  When ParamType is neither a pointer nor a reference, we are dealing with pass-by-value:
+//
+//    template<typename T>
+//    void f(T param);   // param is no passed by value
+//  
+//  This means that param will be a copy of whatever is passed in - completely new object.
+//  The fact that param will be a new object motivates the rules that govern how T is
+//  deduced from expr:
+//
+//    1. As before, if expr's type is a reference, ignore the reference part
+//    2. If, after agnoring expr's reference-ness, expr is const, ingore that too. If it is
+//    volatile, also ingore that. 
+//    Hence:
+//       int x = 27;
+//       const int cx = x;
+//       const int& rx = x;
+//
+//       f(x);   // T's and param's types are both int
+//       f(cx);  // T's and param's types are again both int
+//       f(rx);  // T's and param's types are still both int
+//
+//   Note that even though cx and rx represent const values, param isn't const.
+//   param is an object that's completely independent of cx and rx - a copy of 
+//   cx or rx. The fact that cx and rx can't be modified says nothing about
+//   wether param can be. That's why expr's const-ness (and volatile-ness if any)
+//   is ignored when deducing a type for param: just because expr can't be modified
+//   does not mean that a copy of it can't be.
+//
+//   Notice that const and volatile are ignored only for by-value parameters.
+//   As we've seen, for parameters that are references-to- or pointers-to-const,
+//   the const-ness of expr is preserved during type deduction. But consider the
+//   case where expr is a const pointer to a const object, and expr is passed to a 
+//   by-value param:
+//
+//      template<typename T>
+//      void f(T param);     // param is still passed by value
+//
+//      const char* const ptr = // ptr is const pointer to const object
+//         "Fun with pointers";
+//
+//      f(ptr);   // pass arg of type const char * const
+//
+//   Here, we are dealing with const ptr pointing to const char. When ptr is passed
+//   to f, the bits making up the pointer are copied into param. As such, the pointer
+//   itself (ptr) will be passed by value. In accord with the type deduction rule 
+//   for by-value parameters, the const-ness of ptr will be ingored, and the type 
+//   deduced for param will be const char* i.e. a modifiable pointer to a const char
+//   sring. The const-ness of what ptr points to is preserved during type deducion,
+//   but the const-ness of ptr itself is ignored when copying it to create the new
+//   pointer, param. 
+// 
+//
+//
+//
 
-templat<typename T>
+template<typename T>
 void f_ref(T& param) {
    assert(std::is_pointer<T>::value==false);
    assert(std::is_reference<T>::value==false);
@@ -143,7 +220,14 @@ void f_univ_ref(T&& param) {
   assert(std::is_pointer<T>::value==false);
   std::cout << "T is reference: " << std::is_reference<T>::value << std::endl;
   std::cout << "T is const: " << std::is_const<T>::value << std::endl;
-} 
+};
+
+
+template<typename T>
+void f_copy(T param) {
+  assert(std::is_const<T>::value==false);
+  assert(std::is_volatile<T>::value==false);
+};
 
 int main(const int argc, const char* argv[]) 
 {
