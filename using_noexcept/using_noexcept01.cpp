c70801cbcedb63f2159b52cd7bd548f5f64ffe6e
Dimitar Gueorguiev <dimitar.gueorguiev@celect.com>
2021-07-23 11:08:12 -0400

    noexcept sample 1

---
+++ using_noexcept/using_noexcept01.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+#include <utility>
+#include <memory>
+#include <array>
+#include <assert.h>
+#include <type_traits>
+
+int main(const int argc, const char* argv[]) {
+
+   return 0;
+}
