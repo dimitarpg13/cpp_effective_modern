f57b5e744c9ea1a9e4b654a14377a0fadd8b6e94
Dimitar Gueorguiev <dimitar.gueorguiev@celect.com>
2021-07-23 20:14:31 -0400

    noexcept sample 1

--- using_noexcept/using_noexcept01.cpp
+++ using_noexcept/using_noexcept01.cpp
@@ -23,7 +23,62 @@
 // There is an additional incentive to apply noexcept to functions that won't produce
 // exceptions: it permits compilers to generate better object code. To understand why,
 // let us examine the differences between the C++98 and C++11 ways of saying that a 
-// function won't emit exceptions.  
+// function won't emit exceptions. Consider a function f that promises callers
+// they will never receive an exception. The two ways of expressing that are:
+//
+//    int f(int x) throw();    // no exceptions from f: C++98 style
+//    int f(int x) noexcept;   // no exceptions from f: C++11 style
+//
+// If, at runtime, an exception leaves f, f's exception specification is violated.
+// With the C++98 exception specification, the call stack is unwound to f's caller,
+// and after some actions not relevant here, program execution is terminated. With
+// the C++11 exception specification, runtime behavior is slightly different: the 
+// stack is only possibly unwound before program execution is terminated.
+//
+// The difference between unwinding the call stack and possibly unwinding it has
+// a surprisingly large impact on code generation. In a noexcept function, optimizers
+// need not keep the runtime stack in an unwindable state if an exception would
+// propagate out of the function, nor must they ensure that objects in an noexcept
+// are destroyed in the inverse order of construction should an exception leave the
+// function. Functions with throw() exception specifications lack such optimization 
+// flexibility, as do functions with no exception specification at all. This can be
+// summarized as:
+//
+//    RetType function(params) noexcept;   // most optimizable
+//    RetType function(params) throw();    // less optimizable
+//    RetType function(params);            // less optimizable
+//
+// This alone is a sufficient reason to declare functions noexcept whenver you know
+// they won't produce exceptions.
+//
+// For some functions, the case is even stronger. The move operations are the pre-
+// eminent example. Suppose you have a C++98 code base making use of a 
+// std::vector<Widget>. Widgets are added to the std::vector via push_back:
+//
+//    std::vector<Widget> vw;
+//    ...
+//    Widget w;
+//    ...                // work with w
+//    vw.push_back(w);   // add w to vw
+//    ...
+//
+// Assume this code works fine, and you have no interest in modifying it for C++11.
+// However, you do want to take advantage of the fact that C++11's move semantics
+// can improve the performance of legacy code when move-enabled types are involved.
+// You therefore ensure that Widget has move operations, either by writing them
+// yourself or by seeing to it that the conditions for their automatic generations 
+// are fulfilled. 
+//
+// std::vector::push_back takes advantage of the "move if you can, copy if you must"
+// strategy and it is not the only function in the Stdlib that does. Other functions
+// supporting the strong exception guarantee in C++98 such as std::vector::reserve,
+// std::deque::insert behave in the same way. All these functions replace calls to 
+// copy operations in C++98 with calls to move operations in C++11 only if the move
+// are known not to emit exceptions. But how can a function know if a move operation
+// won't produce exception? The answer is obvious: it checks to see if the operation
+// is declared noexcept.
+//
+//
 
 int main(const int argc, const char* argv[]) {
 
