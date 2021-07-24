e96a5212b798398af41d50550b03bf3532021d96
Dimitar Gueorguiev <dimitar.gueorguiev@celect.com>
2021-07-24 16:36:51 -0400

    auto type deduction sample 1

---
+++ deducing_types/auto_type_deduction01.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+#include <typeinfo>
+#include <type_traits>
+#include <assert.h>
+#include <array>
+
+// Understanding auto type deduction
+//
+//
+int main(const int argc, const char* argv[]) 
+{
+
+
+   return 0;
+}
