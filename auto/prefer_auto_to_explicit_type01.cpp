647f00ee5762cf92d11a39585c9fef8e229bf86a
Dimitar Gueorguiev <dimitar.gueorguiev@celect.com>
2021-08-07 13:25:43 -0400

    prefer auto update

---
+++ auto/prefer_auto_to_explicit_type01.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <typeinfo>
+#include <type_traits>
+#include <assert.h>
+#include <array>
+#include <initializer_list>
+#include <deque>
+
+// Intro:
+// auto is simple but at the same time it is more subtle than it looks.
+// Using it saves typing but also can obstruct correctness and lead
+// to subtle performance issues related to manual type declarations.
+//
+//
+int main(const int argc, const char* argv[]) 
+{
+
+
+   return 0;
+}
