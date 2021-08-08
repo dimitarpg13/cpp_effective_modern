8309c05fd52cec88d9177e8d5d50457a09d6d0d0
Dimitar Gueorguiev <dimitar.gueorguiev@celect.com>
2021-08-07 21:05:00 -0400

    prefer auto to explicit type update

--- auto/prefer_auto_to_explicit_type01.cpp
+++ auto/prefer_auto_to_explicit_type01.cpp
@@ -9,6 +9,7 @@
 #include <initializer_list>
 #include <deque>
 #include <memory>
+#include <functional>
 
 // Intro:
 // auto is simple but at the same time it is more subtle than it looks.
@@ -57,8 +58,36 @@ struct Widget {
 
 auto derefUPLess =                        // comparison func
   [](const std::unique_ptr<Widget>& p1,   // for Widgets
-     const std::unique_ptr<Widget>& p2)  // pointed to by std::unique_ptr's
+     const std::unique_ptr<Widget>& p2)   // pointed to by std::unique_ptr's
   { return *p1 < *p2; };
+
+// In C++14 we can introduce parameters to lambda function which types involve auto:
+
+auto derefLess =                          // C++14 comparison  
+  [](const auto& p1, const auto& p2)      // function for values pointed
+  { return *p1 < *p2; };                  // to by anything pointer-like
+
+// we do not really need auto to declate a variable that holds closure because we
+// can use a std::function object. But what is a std::function object?
+//
+// std::function is a template in the C++11 stdlib that generalizes the idea of a
+// function pointer. Whereas function pointers can point only to functions, 
+// std::function objets can refer to any callable object i.e. anything that can be
+// invoked like a function. Just as you must specify the type of function to point
+// to when you create a function pointer (i.e. the signature of the functions), you
+// must specify the type of the function to refer to when you create a std::function
+// object. You do that through std::function's template parameter. For example, to
+// declare a std::function object named func that could refer to any callable object 
+// acting as if it had this signature:
+// 
+// bool( const std::unique_ptr<Widget>&,  // C++11 signature for std::unique_ptr<Widget>
+//       const std::unique_ptr<Widget>&)  // std::unique_ptr<Widget> comparison function
+// 
+// you'd write this:
+//
+std::function<bool(const std::unique_ptr<Widget>&,
+                   const std::unique_ptr<Widget>&)> func;
+
 
 int main(const int argc, const char* argv[]) 
 {
