0aaeda1ed265a328009a27ff634f3ed1fb8eb270
Dimitar Gueorguiev <dimitar.gueorguiev@celect.com>
2021-08-22 00:13:27 -0400

    moving to modern c++ add

---
+++ moving_to_modern_cpp/init_with_braces_and_parenthesis01.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <iterator>
+#include <typeinfo>
+#include <type_traits>
+#include <assert.h>
+#include <array>
+#include <initializer_list>
+#include <deque>
+#include <memory>
+#include <functional>
+#include <vector>
+#include <unordered_map>
+
+
+
+int main(const int argc, const char* argv[]) 
+{
+
+   return 0;
+}
